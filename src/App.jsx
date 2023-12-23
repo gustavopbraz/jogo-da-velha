c53bf07f7fe00095d6a7385e65e34cb7e2b3edbe
Brunna Louise Martins <brunna.louisemr@gmail.com>
2023-12-23 13:15:08 -0300

    adaptação do placar
    Colocando o placar dentro da página do jogo, adequação de sua lógica para atualizar os valores corretamente e estilização conforme padrões estabelecidos pelo grupo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter , Route, Routes } from 'react-router-dom';
-import Placar from './components/brunna/Placar'
 import Game from './Pages/Game'
 
 function App() {
   return (
-      //<Placar />
+    
     <BrowserRouter>
       <Routes>
         <Route path="/game" element={<Game />} />
